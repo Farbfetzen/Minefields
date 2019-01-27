e1d95ecad8d30a6fd5436706ab460d7f2cbb7426
BastiHz <41594266+BastiHz@users.noreply.github.com>
2019-01-27 16:19:45 +0100

    Change license from MIT to GNU GPL v3

--- source/main.py
+++ source/main.py
@@ -1,12 +1,21 @@
 """Run this file to start the game.
 
-Copyright 2015 Sebastian Henz
-This project is licensed under the MIT license. See file 
-LICENSE for details.
+Copyright (C) 2019 Sebastian Henz
 
-This program was written in Python 3.2.5 using PyGame 1.9.2 
-and should still work with later versions of both.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
+
 
 import sys
 
